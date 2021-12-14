1fa6029ed6e7857acfe240d9cbce9a393ede97d2
Midhat18 <91749902+Midhat18@users.noreply.github.com>
2021-12-14 18:39:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 21 19:05:06 2021
-@author: HP
-"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,render_template
 from TestResultModel import TestResult
-#from TrainChatbotModel import GenerateResponse, intents
+from MeditationRecommendations import recommended_meditation
+from Chatbotfile import getresponse
 import json
 
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 #response = ''
 
 @app.route('/res', methods = ['POST'])
@@ -22,20 +22,32 @@ def testResult():
     answers = requestData['answers']
     return jsonify(result = TestResult(answers))
 
-#@app.route('/chat', methods = ['POST'])
-#def chatResponse():
-    #requestData = request.data
-    #requestData = json.loads(requestData.decode('utf-8'))
-    #userMessage = requestData['message']
-    #return jsonify(response = GenerateResponse(intents, userMessage))
+@app.route('/meditation', methods = ['POST'])
+def meditationrecommendation():
+    requestData = request.data
+    print(requestData)
+    requestData = json.loads(requestData.decode('utf-8'))
+    recommend = requestData['recommend']
+    health_issue=requestData['health_issue']
+    
+    return jsonify(result = recommended_meditation(health_issue,recommend))
+
+@app.route('/chat', methods = ['POST'])
+def chatResponse():
+    requestData = request.data
+    print("chat ")
+    requestData = json.loads(requestData.decode('utf-8'))
+    userMessage = requestData['message']
+    return jsonify(responseUser = getresponse(userMessage))
 
 @app.route('/home', methods = ['GET'])
-def home():
+def home1():
     return jsonify({"message" : "Welcome to EMOBOT"})
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = False)
     
     
 
-#[1,3,3,2,1,3,2]
+#Test[1,3,3,2,1,3,2]
+
